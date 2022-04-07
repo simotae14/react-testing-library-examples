6a1185c23e160e3cc371939a30123bd63eb0a10c
Simone <startae14@gmail.com>
2022-04-07 11:54:05 +0200

    use @testing-library/dom to use the queries method and find the relations between label and input

--- src/components/FavoriteNumber.test.js
+++ src/components/FavoriteNumber.test.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { queries } from '@testing-library/dom';
 import { FavoriteNumber } from './FavoriteNumber';
 
 test('renders a number input with a label "Favorite Number"', () => {
   // create a div to inject the Favorite Number component
   const div = document.createElement('div');
   ReactDOM.render(<FavoriteNumber />, div);
+  // retrieve the Label with the text 'Favorite Number'
+  const input = queries.getByLabelText(div, /favorite number/i);
   // check the input type
-  expect(div.querySelector('input')).toHaveAttribute('type', 'number');
-  // check the label text content
-  expect(div.querySelector('label')).toHaveTextContent('Favorite Number');
+  expect(input).toHaveAttribute('type', 'number');
 });
