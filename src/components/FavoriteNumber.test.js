442f08272a0fc4a4b03f7dbdfc4075db5929eb86
Simone <startae14@gmail.com>
2022-04-07 12:32:44 +0200

    use fireEvent to trigger an event and test the error element using getByRole

--- src/components/FavoriteNumber.test.js
+++ src/components/FavoriteNumber.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { FavoriteNumber } from './FavoriteNumber';
 
 test('renders a number input with a label "Favorite Number"', () => {
@@ -9,4 +9,13 @@ test('renders a number input with a label "Favorite Number"', () => {
   // check the input type
   expect(input).toHaveAttribute('type', 'number');
   debug(input);
+});
+
+test('entering an invalid value shows an error message', () => {
+  const { getByLabelText, getByRole } = render(<FavoriteNumber />);
+  const input = getByLabelText(/favorite number/i);
+  // trigger the onChange event
+  fireEvent.change(input, { target: { value: '10' } });
+  // retrieve the error element
+  expect(getByRole('alert')).toHaveTextContent(/the number is invalid/i);
 });
