01eff2d4d633b718153718dee53687e91a8dbba6
Simone <startae14@gmail.com>
2022-04-07 12:08:51 +0200

    refactor the test to use the render method from @testing-library/react

--- src/components/FavoriteNumber.test.js
+++ src/components/FavoriteNumber.test.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { getQueriesForElement } from '@testing-library/dom';
+import { render } from '@testing-library/react';
 import { FavoriteNumber } from './FavoriteNumber';
 
 test('renders a number input with a label "Favorite Number"', () => {
-  // create a div to inject the Favorite Number component
-  const div = document.createElement('div');
-  ReactDOM.render(<FavoriteNumber />, div);
-  // retrieve the Label with the text 'Favorite Number'
-  const { getByLabelText } = getQueriesForElement(div);
+  const { getByLabelText } = render(<FavoriteNumber />);
   const input = getByLabelText(/favorite number/i);
   // check the input type
   expect(input).toHaveAttribute('type', 'number');
